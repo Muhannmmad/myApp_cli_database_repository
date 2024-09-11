11328b5953688b68e38ca999c68d8cfccfcf7302
Muhammad <muhannmmad@gmail.com>
2024-09-11 12:52:37 +0200

    udates

--- data/database_repository.dart
+++ data/database_repository.dart
@@ -1,1 +1,18 @@
+import '../models/user.dart';
 
+abstract class DatabaseRepository {
+  // Create a new user
+  void addUser(String id, User newUser);
+
+  // Read all users
+  List<String> getAllUserNames();
+
+  // Update user information
+  void updateUser(String id, User updatedUser);
+
+  // Delete a user by ID
+  void deleteUser(String id);
+
+  // Get specific user details
+  User? getUserById(String id);
+}
